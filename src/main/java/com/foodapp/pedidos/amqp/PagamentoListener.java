4f291e6238988b86dc40416956561772037cde06
stephanie ingrid <stephanieingrid5@gmail.com>
2024-07-11 18:44:42 -0300

    Recebe mensagem da fila com Rabbit Listener

---
+++ src/main/java/com/foodapp/pedidos/amqp/PagamentoListener.java
@@ -0,0 +1,17 @@
+package com.foodapp.pedidos.amqp;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PagamentoListener {
+
+    @RabbitListener(queues = "pagamento.concluido")
+    public void recebeMensagem(Message mensagem){
+
+        System.out.println("Pagamento recebido " + mensagem.toString());
+
+    }
+
+}
